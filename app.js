52e362a66f4b105d82cbd0352cdb5b57210d6c9e
Jonathan Geiger <78149229+geiger01@users.noreply.github.com>
2021-03-18 15:13:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const aboutContent = "Yoooo look at my BLOGGGGG i am the globglobgabgalab the schwabellwabbledibblewibbledab dab.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+let blogPosts = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {homeContent: homeStartingContent, posts:blogPosts});
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {aboutContent: aboutContent});
+});
+
+app.get('/contact', function(req, res) {
+
+    res.render('contact', {contactContent: contactContent});
+});
+
+app.get('/compose', function(req, res) {
+
+    res.render('compose');
+});
+
+app.post("/compose", function(req,res){
+
+    let postTitle= req.body.postTitle;
+    let newPost = req.body.newPost;
+
+    var post = {
+      title: postTitle,
+      body:newPost
+    };
+
+    blogPosts.push(post);
+    res.redirect("/");
+});
+
+app.get("/posts/:postName", function(req,res){
+    var urlTitle=_.lowerCase(req.params.postName) //._lowerCase makes the title lower cased
+
+    blogPosts.forEach(function(post){
+      const actualTitle= _.lowerCase(post.title);
+
+      if (actualTitle===urlTitle){
+
+          res.render("post",{title: post.title ,body: post.body})
+
+      }
+    });
+
+});
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
